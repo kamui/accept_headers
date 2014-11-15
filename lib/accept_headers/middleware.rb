2a0c782aa6f7545a5b37b244dc2d16c1f4a7f87e
Jack Chu <kamuigt@gmail.com>
2014-11-15 05:57:00 -0500

    initial rack middleware

---
+++ lib/accept_headers/middleware.rb
@@ -0,0 +1,17 @@
+require "accept_headers"
+
+module AcceptHeaders
+  class Middleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      env["accept_headers.media_types"] = MediaType::Negotiator.new(env["HTTP_ACCEPT"])
+      env["accept_headers.encodings"] = Encoding::Negotiator.new(env["HTTP_ACCEPT_ENCODING"])
+      env["accept_headers.languages"] = Language::Negotiator.new(env["HTTP_ACCEPT_LANGUAGE"])
+
+      @app.call(env)
+    end
+  end
+end
